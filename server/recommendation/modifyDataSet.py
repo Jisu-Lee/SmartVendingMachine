dcdf88be5824bde0818f085d7c24b909cc8320f8
Jang Woojin <ehdwlsdnwls@naver.com>
2018-11-11 10:42:26 +0900

    make rating file and code for that

--- server/recommendation/modifyDataSet.py
+++ server/recommendation/modifyDataSet.py
@@ -1,26 +1,45 @@
 import csv
 from random import uniform
 
-inputFile = open('products.csv','r',encoding='"ISO-8859-1"')
+inputFile = open('products.csv','r',encoding='"UTF-8"')
 iFile = csv.reader(inputFile)
-outputFile = open('newproducts.csv','w',encoding='"UTF-8"')
-oFile = csv.writer(outputFile)
+outputFile1 = open('newproducts.csv','w',encoding='"UTF-8"',newline='')
+oFile1 = csv.writer(outputFile1)
+outputFile2 = open('newratings.csv','w',encoding='"UTF-8"',newline='')
+oFile2 = csv.writer(outputFile2)
 
-for i, line in enumerate(iFile):
-	if(i == 0):
-		outputList = [line[0],line[2],line[3],"price","rating_list"]
-		oFile.writerow(outputList)
-		print(outputList)
-	else:
-		ratingList = []
-		for user in range(100):
-			if(uniform(0,100)>30):
-				ratingList.append(str(user)+":"+str(round(uniform(0,5),1))+',')
-			else:
-				ratingList.append(str(user)+":"+str(0)+',')
-		ratingListStr = ''.join(str(e) for e in ratingList)
-		outputList = [line[0],line[2],line[3][1:-1],round(uniform(10,150),1),'['+ratingListStr[:-1]+']']
-		oFile.writerow(outputList)
+def setNewProductFile():
+	for i, line in enumerate(iFile):
+		if(i == 0):
+			outputList = [line[0],line[2],line[3],"price","rating"]
+			oFile2.writerow(outputList)
+			print(outputList)
+		else:
+			ratingList = []
+			for user in range(100):
+				if(uniform(0,100)>80):
+					ratingList.append(str(user)+":"+str(round(uniform(0,5),1))+',')
+				else:
+					ratingList.append(str(user)+":"+str(0)+',')
+			ratingListStr = ''.join(str(e) for e in ratingList)
+			outputList = [line[0],line[2],line[3][1:-1],round(uniform(10,150),1),'['+ratingListStr[:-1]+']']
+			oFile1.writerow(outputList)
+
+def setNewRatingFile():
+	for i, line in enumerate(iFile):
+		if(i == 0):
+			outputStr = ["User_id","Cos_id","rating"]
+			oFile2.writerow(outputStr)
+			print(outputStr)
+		else:
+			for user in range(100):
+				if(uniform(0,100)>80):
+					outputStr = [str(user),line[0],str(round(uniform(0,5),1))]
+					oFile2.writerow(outputStr)
+				
+
+setNewRatingFile()
 
 inputFile.close()
-outputFile.close()
+outputFile1.close()
+outputFile2.close()
