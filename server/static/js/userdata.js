8cb4f58901217ae0bf4f9393e6ee1edf4e09af97
503 <hezmak0913@naver.com>
2018-11-17 10:57:43 +0900

    change location

---
+++ server/static/js/userdata.js
@@ -0,0 +1,9 @@
+function addUserData(name, age, skinType, gender, ){
+  var template = ;
+  $("#detail").append(template);
+};
+
+$(document).ready(function() {
+addUserData(1, "cosmetic 1", 11037, 3.4, "skin",  true, "왜 안보이지?");
+
+});
